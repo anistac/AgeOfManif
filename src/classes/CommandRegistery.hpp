27b7a2e02b01fcc6a9bd1f379fd7abcc3faf307a
Anis <37740622+anistac@users.noreply.github.com>
2023-06-01 15:31:22 +0200

    Registre de commandes v1
    - mise en place d'un registre de commande pour lister toutes les commandes disponibles

---
+++ src/classes/CommandRegistery.hpp
@@ -0,0 +1,44 @@
+#include "Command.hpp"
+#include <stdexcept>
+#include <typeindex>
+#include <unordered_map>
+
+struct CmdTypeHash {
+  template <typename I1, typename I2>
+  std::size_t operator()(const std::pair<I1,I2> &initiators) const {
+    auto h1 = std::type_index(initiators.first).hash_code();
+    auto h2 = std::type_index(initiators.second).hash_code();
+    return h1 ^ h2;
+  }
+};
+
+
+struct CmdTypeEq {
+  template <typename I1, typename I2>
+  bool operator()(const std::pair<I1, I2> &initiator1, const std::pair<I1, I2> &initiator2) const {
+    return (initiator1.first == initiator2.first && initiator1.second == initiator2.second || initiator1.first == initiator2.second && initiator1.second == initiator2.first);
+  }
+};
+
+class CommandRegistry {
+
+public:
+  template <typename I1, typename I2>
+  static void addCmd(Command *cmd) {
+    std::type_index t1 = typeid(I1);
+    std::type_index t2 = typeid(I2);
+
+    commands[std::make_pair(t1, t2)] = cmd;
+  }
+  template <typename I1, typename I2>
+  static Command* findAvailableCmds(const I1 init1, const I2 init2) {
+    auto it = commands.find(std::make_pair(std::type_index(init1), std::type_index(init2)));
+    if (it != commands.end()) {
+      return it->second;
+    } else {
+      throw std::runtime_error("Command not found");
+    }
+  }
+private:
+  static std::unordered_map<std::pair<std::type_index, std::type_index>, Command*, CmdTypeHash, CmdTypeEq> commands;
+};
