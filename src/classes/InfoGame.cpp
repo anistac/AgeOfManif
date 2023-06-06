33c2eed803d345d2895fae68725de9901f47036f
Anis <37740622+anistac@users.noreply.github.com>
2023-06-07 00:13:46 +0200

    MAJ InfoGame: action restantes + Tour
    infogame affiche maintenant le nombre d'actions restantes et le numéro du tour

--- src/classes/InfoGame.cpp
+++ src/classes/InfoGame.cpp
@@ -1,6 +1,7 @@
 #include "InfoGame.hpp"
 #include <iostream>
 #include "FontManager.hpp"
+#include "Invoker.hpp"
 
 void InfoGame::updateInfo(float _opinion, int _argentPeuple, int _argentGouv) {
     _rectangleBg.setSize(sf::Vector2f(900, 40));
@@ -20,7 +21,29 @@ void InfoGame::updateInfo(float _opinion, int _argentPeuple, int _argentGouv) {
 void InfoGame::draw(sf::RenderTarget& window, sf::RenderStates states) const {
     sf::View currView = window.getView();
     window.setView(window.getDefaultView());
+    sf::RectangleShape turnInfoBox(sf::Vector2f(200, 200));
+    turnInfoBox.setPosition(window.getSize().x - 200, 40);
+    turnInfoBox.setFillColor(sf::Color::White);
+    std::string txt = "Tour : " + std::to_string(Invoker::getCurrentTurn());
+
+    sf::Text turnInfoText;
+    turnInfoText.setString(txt);
+    turnInfoText.setPosition(window.getSize().x - 200 + 5, 60);
+    turnInfoText.setFont(FontManager::getInstance().getFont());
+    turnInfoText.setCharacterSize(20);
+    turnInfoText.setFillColor(sf::Color::Black);
+    sf::Text remainingActionText;
+    txt = "Actions restantes : " + std::to_string(Invoker::getRemainingActions());
+    remainingActionText.setString(txt);
+    remainingActionText.setPosition(window.getSize().x - 200 + 5, 80);
+    remainingActionText.setFont(FontManager::getInstance().getFont());
+    remainingActionText.setCharacterSize(20);
+    remainingActionText.setFillColor(sf::Color::Black);
+    window.draw(turnInfoBox, states);
     window.draw(_rectangleBg, states);
     window.draw(_text, states);
+    window.draw(turnInfoBox, states);
+    window.draw(turnInfoText, states);
+    window.draw(remainingActionText, states);
     window.setView(currView);
 }
--- src/classes/Invoker.hpp
+++ src/classes/Invoker.hpp
@@ -1,24 +1,27 @@
 #ifndef INVOKER_HPP
 #define INVOKER_HPP
 
-#include <vector>
-#include <memory>
-#include "Command.hpp"
 #include "ActionType.hpp"
+#include "Command.hpp"
+#include <memory>
+#include <vector>
 // #include "Region.hpp"
 class Region;
 // Classe Invoker
 class Invoker {
 public:
-    static void addCommand(std::shared_ptr<Command> command);
-    static bool executeCommand(Action action);
-    static void setCurrentRegion(Region *reg);
-    static Region* getCurrentRegion() { return _reg; }
-    static int getCurrentCamp() { return currentTurn % 2; }
+  static void addCommand(std::shared_ptr<Command> command);
+  static bool executeCommand(Action action);
+  static void setCurrentRegion(Region *reg);
+  static Region *getCurrentRegion() { return _reg; }
+  static int getCurrentCamp() { return currentTurn % 2; }
+  static int getCurrentTurn() { return currentTurn + 1; }
+  static int getRemainingActions() { return _remainingActions; }
+
 private:
-    inline static std::vector<std::shared_ptr<Command>> commands;
-    inline static Region* _reg;
-    inline static int _remainingActions = 3;
-    inline static int currentTurn = 0;
+  inline static std::vector<std::shared_ptr<Command>> commands;
+  inline static Region *_reg;
+  inline static int _remainingActions = 3;
+  inline static int currentTurn = 0;
 };
 #endif // INVOKER_HPP
